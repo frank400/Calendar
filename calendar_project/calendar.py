cac522b2a36dc19779c794f6aa6e55e797ef5caa
João Victor Huguenin <46404918+frank400@users.noreply.github.com>
2019-12-28 19:51:34 -0300

    everything(code,and json file of google calendar)
    the script in python and the json file that API uses

---
+++ calendar_project/calendar.py
@@ -0,0 +1,71 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+
+def split_time(datetime):
+    datetime=datetime.split('-')
+    datetime=datetime[2].split('T')
+    return datetime[1]
+
+def split_date(dateTime):
+    date=dateTime.split('T')[0]
+    return date    
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    today=split_date(datetime.datetime.today().isoformat())
+    print('these are all yours tasks for today\n\n***--------------***')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                          maxResults=10, singleEvents=True,
+                                          orderBy='startTime').execute()
+    events = events_result.get('items', [])
+    
+    if not events:
+        print('No upcoming events found.')
+    for event in events:        
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        end = event['end'].get('dateTime', event['end'].get('date'))
+        date=split_date(start)
+        if date==today:
+            Time_end=split_time(end)
+            Time_start=split_time(start)
+            
+            print(f"Nome: {event['summary']}\nComeço: {Time_start} \nFim: {Time_end}\n***--------------***\n")
+    
+        
+
+
+if __name__ == '__main__':
+    main()
